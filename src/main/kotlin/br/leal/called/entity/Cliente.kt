0b25a102de55ce23d0b508398bffba6476663cf1
leal <matheusskeal98@gmail.com>
2023-09-21 15:44:33 -0300

    create entity e as migrations cliente.

---
+++ src/main/kotlin/br/leal/called/entity/Cliente.kt
@@ -0,0 +1,16 @@
+package br.leal.called.entity
+
+import jakarta.persistence.*
+
+@Table(name = "tb_cliente")
+@Entity
+class Cliente (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+    @Column(name = "nome")
+    var nome: String? = null,
+    @Column(name = "email")
+    var email: String? = null,
+    @Column(name = "cpf")
+    var cpf: String? = null
+)
---
+++ src/main/resources/migrations/V2__CREATE_TABLE_CLIENTE.sql
@@ -0,0 +1,6 @@
+CREATE TABLE nome_da_tabela (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255),
+    email VARCHAR(255),
+    cpf VARCHAR(14)
+);
