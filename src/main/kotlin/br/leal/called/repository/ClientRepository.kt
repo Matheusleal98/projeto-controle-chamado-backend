819a1f302fc9ab56101046cbdb1b976ac602e801
leal <matheusskeal98@gmail.com>
2023-09-27 15:16:57 -0300

    criando função no ClientRepository para buscar cliente pelo email.

--- src/main/kotlin/br/leal/called/repository/ClientRepository.kt
+++ src/main/kotlin/br/leal/called/repository/ClientRepository.kt
@@ -3,7 +3,10 @@ package br.leal.called.repository
 import br.leal.called.entity.Cliente
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
 
 @Repository
 open interface ClientRepository: JpaRepository<Cliente, Long> {
+
+    fun findByEmail(email: String): Optional<Cliente>
 }
